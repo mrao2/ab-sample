d2fea2e0abcd62501a76f74a7501179da4e8c611
Mohith Rao <mohith.rao96@gmail.com>
2018-11-11 20:09:15 -0600

    added redux (non Functional)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/configureStore';
 import App from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
